d02f3cc3c910fea2c31d72ca18d46fba9f67d8c3
Cappy Ishihara <cappy@cappuchino.xyz>
2025-12-07 03:41:29 +0700

    use hard tabs wtf

--- src/database/server.ts
+++ src/database/server.ts
@@ -10,20 +10,20 @@ import {
 import { env } from "../env";
 
 export default class ServerData {
-  private static prisma = new PrismaClient();
-  private prisma: PrismaClient;
+	private static prisma = new PrismaClient();
+	private prisma: PrismaClient;
 
-  constructor() {
-    // reuse the singleton
-    this.prisma = ServerData.prisma;
-  }
+	constructor() {
+		// reuse the singleton
+		this.prisma = ServerData.prisma;
+	}
 
-  public async get(guildId: string): Promise<Guild> {
-    return (
-      (await this.prisma.guild.findUnique({ where: { guildId } })) ??
-      this.createGuild(guildId)
-    );
-  }
+	public async get(guildId: string): Promise<Guild> {
+		return (
+			(await this.prisma.guild.findUnique({ where: { guildId } })) ??
+			this.createGuild(guildId)
+		);
+	}
 
 	private async createGuild(guildId: string): Promise<Guild> {
 		return await this.prisma.guild.upsert({
