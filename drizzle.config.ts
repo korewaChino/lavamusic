3ada92d17e192a01f14015545aec16da396a2d30
Cappy Ishihara <cappy@cappuchino.xyz>
2026-02-07 18:20:35 +0700

    fix: Automatically detect postgres driver from URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,14 @@ import { env } from "./src/env";
 
 if (!env.DATABASE_URL) env.DATABASE_URL = "file:./lavamusic-pgdata";
 
+const isPgLite =
+  !env.DATABASE_URL.startsWith("postgres://") &&
+  !env.DATABASE_URL.startsWith("postgresql://");
+
 export default defineConfig({
-	out: "./drizzle/postgres",
-	schema: "./src/database/schemas.ts",
-	dialect: "postgresql",
-	driver: "pglite",
-	dbCredentials: { url: env.DATABASE_URL },
+  out: "./drizzle/postgres",
+  schema: "./src/database/schemas.ts",
+  dialect: "postgresql",
+  driver: isPgLite ? "pglite" : "pg",
+  dbCredentials: { url: env.DATABASE_URL },
 });
